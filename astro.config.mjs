b050bdd7b80f5d7eab44e6f1c8569cd5193dcb7c
PichardoWilfred <pichardowilfred@gmail.com>
2023-08-11 20:12:02 -0400

    scarlet logo as vue component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import vue from "@astrojs/vue";
 
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), vue()],
 });
