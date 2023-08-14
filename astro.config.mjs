f95096d7ca6822971033c167abb3b3a6784058ec
PichardoWilfred <pichardowilfred@gmail.com>
2023-08-14 13:52:46 -0400

    testing different deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
-const config = {
-  site:  process.env.GH_SITE,
-  base:  process.env.GH_BASE,
-}
 export default defineConfig({
   integrations: [tailwind(), vue()],
-  ...(process.env.GH_ENV) ? config : {}
+  ...(process.env.GH_ENV) ? {
+      site:  process.env.GH_SITE,
+      base:  process.env.GH_BASE,
+    } : {}
 });
 
