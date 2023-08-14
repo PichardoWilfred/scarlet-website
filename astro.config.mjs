1f82f3c13157b77005e436c486b76f13ee57c7e2
PichardoWilfred <pichardowilfred@gmail.com>
2023-08-14 13:39:04 -0400

    correcting gh-actions deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
-const base_config = {
-}
 const config = {
   site:  process.env.GH_SITE,
   base:  process.env.GH_BASE,
 }
 export default defineConfig({
   integrations: [tailwind(), vue()],
-  ...GH_ENV ? config : {}
+  ...(process.env.GH_ENV) ? config : {}
 });
 
