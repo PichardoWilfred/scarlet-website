1b6c22b8f6d049e54e6fdee712a171d51213f9aa
PichardoWilfred <pichardowilfred@gmail.com>
2023-08-14 13:35:42 -0400

    deploy .env configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
-
+const base_config = {
+}
+const config = {
+  site:  process.env.GH_SITE,
+  base:  process.env.GH_BASE,
+}
 export default defineConfig({
   integrations: [tailwind(), vue()],
-  site:  process.env.GH_SITE || '',
-  base:  process.env.GH_BASE || '',
+  ...GH_ENV ? config : {}
 });
 
